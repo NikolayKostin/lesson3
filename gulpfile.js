173ce393c3625a4cec8a4dc04a895f0f89f39a30
Nik <nikolaymetos@gmail.com>
2017-04-02 23:37:01 +0300

    4th complete

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,32 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
-const jade = require('gulp-jade');
 const notify = require('gulp-notify');
+const pug = require('gulp-pug');
+var paths = {
+  scss:['./scss/main.scss'],
+  pug:['./*.pug']
+};
 
-gulp.task('jade',function(done){
-   gulp.src('index.jade')
+gulp.task('views', function buildHTML() {
+   return gulp.src('./*.pug')
     .on('data',function(file){
         console.log({
             relative: file.relative,
             contents: file.contents
         })
     })
-        .pipe(jade())
+        .pipe(pug())
         .pipe(gulp.dest('./dist'));
 done();
 });
+
+
+/*
+gulp.task('views', function buildHTML() {
+  return gulp.src('views/*.pug')
+  .pipe(pug({
+    // Your options in here.
+  }))
+});
+*/
